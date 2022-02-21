22f150e144146c9e2a8db955ba2114a909e3b267
TejasGMahajan <100075661+TejasGMahajan@users.noreply.github.com>
2022-02-21 22:33:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+//Q.1 For the given JSON iterate over all for loops (for, for in, for of, forEach)
+
+
+var users = {
+	name : ["Tony" , "steve" , "peter", "Loki", "Thor" , "strange"]
+  };
+  
+  //json example through for loop 
+   for(var i = 0; i<users.name.length; i++){
+	 console.log(users.name[i]);
+   }
+  
+  //json example through for in loop 
+  for (const marvel in users) {
+	for (const Heros in users[marvel]) {
+	  console.log(users[marvel][Heros]);
+	}
+  }
+  
+  //json example through for of loop 
+  
+   for (const marvel of users.name) {
+	 console.log(marvel);
+   }
+  
+  
+  //json example through forEach loop 
+	users.name.forEach( u => {
+	  console.log(u)
+	});
+	
+	//Q.2 Create your own resume data in JSON format
+
+	var resume=[{"Name":"Hariharan.U.S",
+             "qualification":"B.E",
+             "email":"hari@gmail.com",
+            "Experience":"fresher",
+        "Address":"abc",
+    "Domain":"Full stack development",}];
+ //console.log(resume);
+resume.forEach(function() {
+    console.log(resume);
+});
